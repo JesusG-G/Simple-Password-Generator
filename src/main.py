2dc5562876152e7075b9b14e809a0738bae2b766
JesusG-G <jesusguillermogonzalezguaita@gmail.com>
2023-07-13 16:17:48 -0400

    [JESUS] - init_generator function created and the logic for the inputs

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 #To use the sting pre configurated
 import string
 #Module to generete secure random numbers, and ohter features
@@ -26,7 +27,7 @@ def generate_password(length: int, symbols: bool, uppercase: bool) -> str :
     new_password: str = ''
     for _ in range(length):
         new_password += combination[secrets.randbelow(combination_length)]
-    print(new_password)
+
     if not contains_symbols(new_password):
         random_symbols: str = secrets.choice(string.punctuation)
         new_password_length: int = len(new_password)
@@ -43,7 +44,31 @@ def generate_password(length: int, symbols: bool, uppercase: bool) -> str :
     
     return new_password
 
+def init_genertator():
+    while True:
+        print("Welcome to the password generator.")
+        print('To exit introduce "exit".')
+        print("----------------------------------------------")
+        quantity_password: str = input("How much passwords do you want to generate? >> ")
+        length_password: str = input("What is the length of the password? >> ")
+        print("----------------------------------------------")
+
+        if quantity_password == 'exit' or length_password == 'exit':
+            print("Thanks for using the program!!!")
+            sys.exit()
+        if not quantity_password.isnumeric() or not length_password.isnumeric():
+            print("Please introduce a number.")
+        elif int(quantity_password) < 1:
+            print("Please introduce a bigger number")
+        elif int(length_password) < 4:
+            print("Please the length of the password(s) need to be more than 5") 
+        else:
+            break
+    for i in range(0, int(quantity_password)):
+        new_pass: str = generate_password(length=int(length_password),symbols=True,uppercase=True)
+        print(f"{i+1} -> {new_pass} (U: {contains_upper(new_pass)} S: {contains_symbols(new_pass)})")
+
+
 if __name__ == "__main__":
-    for i in range(1,2):
-        new_pass: str = generate_password(length=3,symbols=True,uppercase=True)
-        print(f"{i} -> {new_pass} (U: {contains_upper(new_pass)} S: {contains_symbols(new_pass)})")
+    init_genertator()
+        
